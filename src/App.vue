1caf734ca9a97927d3214ee9e87580fe5e1ee25a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-21 15:35:30 +0100

    created and imported in vue.app AppHome and AppTechnologies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppProjects from './pages/AppProjects.vue';
+import AppHome from './pages/AppHome.vue';
+import AppTechnologies from './pages/AppTechnologies.vue';
 import AppFooter from './components/AppFooter.vue';
 export default {
   components: {
     AppHeader,
+    AppHome,
     AppProjects,
+    AppTechnologies,
     AppFooter,
 
   }
