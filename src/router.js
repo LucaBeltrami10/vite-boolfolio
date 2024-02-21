ff9671b3dd1cb6f4e4e9275f70e4dfcb6ecc91f0
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-21 15:47:13 +0100

    added routes

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from './pages/HomePage.vue';
+import ProjectList from './pages/ProjectsList.vue';
+import TechnologiesList from './pages/TechnologiesList.vue';
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage,
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: ProjectList,
+        },
+        {
+            path: '/technologies',
+            name: 'technologies',
+            component: TechnologiesList,
+        },
+    ],
+});
