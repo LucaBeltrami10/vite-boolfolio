b43b847a681bdb15fc910e3ac218011bc33c9412
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-23 09:41:54 +0100

    inserted router link in project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,6 +5,11 @@
             <li class="list-group-item">{{ project.description }}</li>
             <li class="list-group-item">{{ project.status = 0 ? 'in corso' : 'finito' }}</li>
             <li class="list-group-item">{{ project.type.type }}</li>
+            <li class="list-group-item text-center">
+                <router-link :to="linkRoute" class="nav-link">
+                    <button type="button" class="btn btn-dark">Visualizza</button>
+                </router-link>
+            </li>
         </ul>
     </div>
 </template> 
@@ -13,6 +18,7 @@
 export default {
     props: [
         'project',
+        'linkRoute',
     ]
 
 }
